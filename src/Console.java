88cd5cd5d84c68687be4dc76764a20dab1489209
l0stfake7 <bartek.poniatyszyn@gmail.com>
2016-05-24 13:24:31 +0200

    Update 1.0.2 - IntelliJ files

---
+++ src/Console.java
@@ -0,0 +1,8 @@
+/**
+ * Created by bartek on 24.05.16.
+ */
+public class Console {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
---
+++ src/Window.java
@@ -0,0 +1,5 @@
+/**
+ * Created by bartek on 24.05.16.
+ */
+public class Window {
+}

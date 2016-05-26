6ad6ae688ae8069521be8d8031b4afc1e4e23599
l0stfake7 <bartek.poniatyszyn@gmail.com>
2016-05-26 17:00:29 +0200

    Update 1.0.5 - add console menu

--- src/Console.java
+++ src/Console.java
@@ -1,11 +1,44 @@
+import java.util.Scanner;
+
 /**
  * Created by bartek on 24.05.16.
  */
 public class Console {
+
     public static void main(String[] args) {
 
-        try {
+        try (Scanner scanner = new Scanner(System.in)){//scanner close automatticly with end of try body
             System.out.println("JBank v1.0.0");
+            //write menu here\
+
+            boolean exitFlag = false;
+            while(!exitFlag) {
+                System.out.println("Menu:\n\n1. Customer\n2. Order\n3. List of Products\n4. Exit\n\nType number");
+                short number = scanner.nextShort();
+
+                if(number > 0 && number < 5) {
+                    switch(number) {
+                        case 1: {
+                            break;
+                        }
+                        case 2: {
+                            break;
+                        }
+                        case 3: {
+                            break;
+                        }
+                        case 4: {
+                            exitFlag = true;
+                            break;
+                        }
+                        default: continue;//never run :d?
+                    }
+                }
+                else {
+                    continue;
+                }
+            }
+
         }
         catch(Exception exc) {
             System.out.println("Exception: " + exc);

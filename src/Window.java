7949af46d3db8978531aa0485b1c99e477bf8f7b
l0stfake7 <bartek.poniatyszyn@gmail.com>
2016-06-04 15:44:45 +0200

    Update 1.0.11 - remove forms

--- src/Window.java
+++ src/Window.java
@@ -1,11 +1,8 @@
-import Forms.MainForm;
-
 /**
  * Created by bartek on 24.05.16.
  */
 public class Window {
     public static void main(String[] args) {
         //window constructor here...in future :D
-        MainForm mainForm = new MainForm();
     }
 }
